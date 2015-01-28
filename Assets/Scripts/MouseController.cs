af2164e904c20fb67f10ce080f9ed90c22c38b40
Jose Luis Molina <jldemolina@gmail.com>
2015-01-28 17:21:38 +0000

    Infinity scene script included

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class MouseController : MonoBehaviour {
 	public float jetpackForce = 75.0f;
+	public float forwardMovementSpeed = 3.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,5 +21,9 @@ public class MouseController : MonoBehaviour {
 		if (jetpackActive) {
 			rigidbody2D.AddForce(new Vector2(0, jetpackForce));
 		}
+
+		Vector2 newVelocity = rigidbody2D.velocity;
+		newVelocity.x = forwardMovementSpeed;
+		rigidbody2D.velocity = newVelocity;
 	}
 }
