ddaf5e623287cdadb74a60be629bd8398c4a0540
Jose Luis Molina <jldemolina@gmail.com>
2015-01-28 18:29:07 +0000

    Animations added!

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -5,9 +5,17 @@ public class MouseController : MonoBehaviour {
 	public float jetpackForce = 75.0f;
 	public float forwardMovementSpeed = 3.0f;
 
+	public Transform groundCheckTransform;
+	private bool grounded;
+	public LayerMask groundCheckLayerMask;
+	Animator animator;
+
+	public ParticleSystem jetpack;
+
+
 	// Use this for initialization
 	void Start () {
-
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -25,5 +33,20 @@ public class MouseController : MonoBehaviour {
 		Vector2 newVelocity = rigidbody2D.velocity;
 		newVelocity.x = forwardMovementSpeed;
 		rigidbody2D.velocity = newVelocity;
+
+		UpdateGroundedStatus();
+		AdjustJetpack(jetpackActive);
 	}
+
+	void UpdateGroundedStatus() {
+		grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+		animator.SetBool("grounded", grounded);
+	}
+
+	void AdjustJetpack (bool jetpackActive) {
+		jetpack.enableEmission = !grounded;
+		jetpack.emissionRate = jetpackActive ? 300.0f : 75.0f; 
+	}
+
+
 }
